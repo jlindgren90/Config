238b2e8a4ab33030eb215cb59222a7eab2ad0de0
John Lindgren <john@jlindgren.net>
2018-12-10 23:59:21 -0500

    Collect a bunch of Linux config files together.

---
+++ clutter.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+pacman -Scc
+
+gtk-update-icon-cache /usr/share/icons/gnome
+gtk-update-icon-cache /usr/share/icons/hicolor
+
+echo / > inst.list
+pacman -Qql | sed 's/\/$//' | sort -u >> inst.list
+
+find / -xdev | sort | sed -e '/^\/dev\//d' -e '/^\/proc\//d' -e '/^\/sys\//d' > files.list
+
+diff inst.list files.list | ./shorten > files.diff
+rm inst.list files.list
