15e8eec6ebd86ce7761a90724efbd7e92a069756
John Lindgren <john@jlindgren.net>
2021-07-13 21:44:06 -0400

    Simplify quick-settings using std::shared_ptr

--- tools/quick-settings.cc
+++ tools/quick-settings.cc
@@ -6,78 +6,49 @@
 #include <memory>
 #include <stdio.h>
 
-template<typename T>
-using AutoPtr = std::unique_ptr<T, void (*)(T *)>;
-
-struct CharPtr : public std::unique_ptr<char, void (*)(void *)>
+template<typename T, typename D>
+static std::shared_ptr<T> to_shared(T * ptr, D deleter)
 {
-    CharPtr(char * val) : unique_ptr(val, g_free) {}
-};
+    return std::shared_ptr<T>(ptr, deleter);
+}
 
-class CommandAction : public QAction
+static void showMenu(GKeyFile * inifile, const QPoint & pos)
 {
-public:
-    CommandAction(const char * name, CharPtr && command, QObject * parent)
-        : QAction(name, parent), command(std::move(command))
-    {
-        connect(this, &QAction::triggered, this, &CommandAction::run);
-    }
-
-private:
-    CharPtr command;
-    void run() { system(command.get()); }
-};
-
-class QuickSettingsIcon : public QSystemTrayIcon
-{
-public:
-    QuickSettingsIcon(AutoPtr<GKeyFile> && inifile)
-        : QSystemTrayIcon(QIcon::fromTheme("preferences-system")),
-          inifile(std::move(inifile))
-    {
-        connect(this, &QSystemTrayIcon::activated, this,
-                &QuickSettingsIcon::activate);
-    }
-
-private:
-    AutoPtr<GKeyFile> inifile;
-    void activate(QSystemTrayIcon::ActivationReason reason);
-};
-
-void QuickSettingsIcon::activate(QSystemTrayIcon::ActivationReason reason)
-{
-    if (reason != QSystemTrayIcon::Trigger)
-        return;
-
     auto menu = new QMenu();
-    auto items = AutoPtr<char *>(g_key_file_get_groups(inifile.get(), nullptr),
-                                 g_strfreev);
+    auto items = to_shared(g_key_file_get_groups(inifile, nullptr), g_strfreev);
 
-    for (char ** i = items.get(); *i != nullptr; i++)
+    for (char ** i = items.get(); *i; i++)
     {
         const char * item = *i;
-        auto query = CharPtr(
-            g_key_file_get_value(inifile.get(), item, "query", nullptr));
-        auto enable = CharPtr(
-            g_key_file_get_value(inifile.get(), item, "enable", nullptr));
-        auto disable = CharPtr(
-            g_key_file_get_value(inifile.get(), item, "disable", nullptr));
-        auto command = CharPtr(
-            g_key_file_get_value(inifile.get(), item, "command", nullptr));
+        auto query = to_shared(
+            g_key_file_get_value(inifile, item, "query", nullptr), g_free);
+        auto enable = to_shared(
+            g_key_file_get_value(inifile, item, "enable", nullptr), g_free);
+        auto disable = to_shared(
+            g_key_file_get_value(inifile, item, "disable", nullptr), g_free);
+        auto command = to_shared(
+            g_key_file_get_value(inifile, item, "command", nullptr), g_free);
 
         if (query && enable && disable)
         {
             bool enabled = (system(query.get()) == 0);
-            auto action = new CommandAction(
-                item, enabled ? std::move(disable) : std::move(enable), menu);
+            auto toggle = enabled ? disable : enable;
+            auto action = new QAction(item, menu);
 
             action->setCheckable(true);
             action->setChecked(enabled);
+            QObject::connect(action, &QAction::triggered,
+                             [toggle]() { system(toggle.get()); });
+
             menu->addAction(action);
         }
         else if (command)
         {
-            menu->addAction(new CommandAction(item, std::move(command), menu));
+            auto action = new QAction(item, menu);
+            QObject::connect(action, &QAction::triggered,
+                             [command]() { system(command.get()); });
+
+            menu->addAction(action);
         }
         else
         {
@@ -87,16 +58,17 @@ void QuickSettingsIcon::activate(QSystemTrayIcon::ActivationReason reason)
     }
 
     menu->setAttribute(Qt::WA_DeleteOnClose);
-    menu->popup(geometry().topLeft());
+    menu->popup(pos);
 }
 
 int main(int argc, char ** argv)
 {
     QApplication app(argc, argv);
 
-    auto inifile = AutoPtr<GKeyFile>(g_key_file_new(), g_key_file_unref);
-    auto inipath = CharPtr(g_build_filename(g_get_user_config_dir(),
-                                            "quick-settings.ini", nullptr));
+    auto inifile = to_shared(g_key_file_new(), g_key_file_unref);
+    auto inipath = to_shared(g_build_filename(g_get_user_config_dir(),
+                                              "quick-settings.ini", nullptr),
+                             g_free);
 
     if (!g_key_file_load_from_file(inifile.get(), inipath.get(),
                                    G_KEY_FILE_NONE, nullptr))
@@ -105,7 +77,15 @@ int main(int argc, char ** argv)
         return 1;
     }
 
-    QuickSettingsIcon icon(std::move(inifile));
+    QSystemTrayIcon icon(QIcon::fromTheme("preferences-system"));
+
+    QObject::connect(&icon, &QSystemTrayIcon::activated,
+                     [&icon, inifile](QSystemTrayIcon::ActivationReason reason)
+                     {
+                         if (reason == QSystemTrayIcon::Trigger)
+                             showMenu(inifile.get(), icon.geometry().topLeft());
+                     });
+
     icon.show();
 
     return app.exec();
