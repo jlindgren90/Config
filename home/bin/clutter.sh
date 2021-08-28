50b554e23f7c02e10372834efd9cd7ecb9daf0c5
John Lindgren <john@jlindgren.net>
2021-08-28 10:31:50 -0400

    Tweak clutter.sh

--- home/bin/clutter.sh
+++ home/bin/clutter.sh
@@ -6,7 +6,8 @@ gtk-update-icon-cache /usr/share/icons/gnome
 gtk-update-icon-cache /usr/share/icons/hicolor
 
 echo / > inst.list
-pacman -Qql | sed 's/\/$//' | sort -u >> inst.list
+# /etc/ssl/certs is messy, shorten it
+pacman -Qql | sed -e 's/\/$//' -e '/^\/etc\/ssl\/certs/d' | sort -u >> inst.list
 
 find / -xdev | sort | sed -e '/^\/dev\//d' -e '/^\/proc\//d' -e '/^\/sys\//d' > files.list
 
