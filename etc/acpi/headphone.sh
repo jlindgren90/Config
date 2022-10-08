5e221ddbdc3ad578a643609e53e16299156c33a1
John Lindgren <john@jlindgren.net>
2022-10-08 12:20:40 -0400

    Limit headphone volume

--- etc/acpi/headphone.sh
+++ etc/acpi/headphone.sh
@@ -1,11 +1,14 @@
 #!/bin/sh
 case "$3" in
     plug)
-        aeq disable >> /dev/null
-        amixer set Speaker mute >> /dev/null
+        aeq disable > /dev/null
+        amixer set Headphone -- -18dB > /dev/null
+        amixer set Headphone unmute > /dev/null
+        amixer set Speaker mute > /dev/null
         ;;
     unplug)
-        aeq enable >> /dev/null
-        amixer set Speaker unmute >> /dev/null
+        aeq enable > /dev/null
+        amixer set Headphone mute > /dev/null
+        amixer set Speaker unmute > /dev/null
         ;;
 esac
