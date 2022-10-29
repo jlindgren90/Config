8ffbe8bf9d0715c4fa24e79f55d9a9da5c75516c
John Lindgren <john@jlindgren.net>
2022-10-29 12:04:06 -0400

    Stop muting/unmuting automatically

--- etc/acpi/headphone.sh
+++ etc/acpi/headphone.sh
@@ -2,13 +2,8 @@
 case "$3" in
     plug)
         aeq disable > /dev/null
-        amixer set Headphone -- -18dB > /dev/null
-        amixer set Headphone unmute > /dev/null
-        amixer set Speaker mute > /dev/null
         ;;
     unplug)
         aeq enable > /dev/null
-        amixer set Headphone mute > /dev/null
-        amixer set Speaker unmute > /dev/null
         ;;
 esac
