4b6a651a3bc8499b5e8f9de0efc296220279518f
John Lindgren <john@jlindgren.net>
2023-03-18 11:38:07 -0400

    List AUR vs. local packages separately

--- home/bin/pkgreport.sh
+++ home/bin/pkgreport.sh
@@ -10,10 +10,15 @@ pacman -Qqett >> pkgreport.txt
 printf "\n\n*** ORPHANED DEPENDENCIES ***\n\n" >> pkgreport.txt
 pacman -Qqdtt >> pkgreport.txt
 
-printf "\n\n*** NOT IN OFFICIAL REPOS ***\n\n" >> pkgreport.txt
-pacman -Qqm | tee pkgunknown.txt >> pkgreport.txt
+printf "\n\n*** AVAILABLE FROM AUR ***\n\n" >> pkgreport.txt
+pacman -Qm | aur vercmp --all | cut -d' ' -f1 | tee pkgaur.txt >> pkgreport.txt
 
-printf "\n\n*** LOCAL OVERRIDES ***\n\n" >> pkgreport.txt
+printf "\n\n*** INSTALLED LOCALLY ***\n\n" >> pkgreport.txt
+pacman -Qqm > pkgunknown.txt
+comm -23 pkgunknown.txt pkgaur.txt >> pkgreport.txt
+rm pkgaur.txt
+
+printf "\n\n*** OVERRIDDEN LOCALLY ***\n\n" >> pkgreport.txt
 sed -n '/%NAME%/{n;h};/%VALIDATION%/{n;/none/{x;p}}' /var/lib/pacman/local/*/desc > pkgunsigned.txt
 comm -23 pkgunsigned.txt pkgunknown.txt >> pkgreport.txt
 rm pkgunsigned.txt pkgunknown.txt
