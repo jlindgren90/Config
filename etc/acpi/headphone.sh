302df5f69bce6155f3e5030c121678b1c58b1c66
John Lindgren <john@jlindgren.net>
2025-01-18 14:32:16 -0500

    Fix headphone auto mute/unmute

--- etc/acpi/headphone.sh
+++ etc/acpi/headphone.sh
@@ -2,8 +2,12 @@
 case "$3" in
     plug)
         aeq disable > /dev/null
+        amixer sset Speaker mute
+        amixer sset Headphone unmute
         ;;
     unplug)
         aeq enable > /dev/null
+        amixer sset Headphone mute
+        amixer sset Speaker unmute
         ;;
 esac
